3473e7f2a38629a9d7c2c9005a0aef2804d7e83a
dbrudner <dbrudne1@gmail.com>
2018-03-21 14:13:51 -0400

    commit

--- react/src/post-keyboard/post.js
+++ react/src/post-keyboard/post.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
 import styled from 'styled-components'
 
-const PostContainer = styled.div``
+const PostContainer = styled.div`
+    margin-left: 40%;
+    font-size: 2rem;
+`
+
+const Label = styled.span`
+    margin-right: 3rem;
+`
 
 class Post extends Component {
     constructor(props) {
@@ -27,12 +34,17 @@ class Post extends Component {
 
     renderInput = name => {
         return (
-            <input key={name} value={this.state.keyboard[name]} type='text' onChange={event => this.handleChange(name, event.target.value)} />            
+            <div>
+                <Label>
+                    {name.charAt(0).toUpperCase() + name.substr(1)}
+                </Label>
+                <input key={name} value={this.state.keyboard[name]} type='text' onChange={event => this.handleChange(name, event.target.value)} />            
+            </div>
         )
     }
 
     renderAllInputs = () => {
-        return Object.keys(this.state.keyboard).map((item, i) => {
+        return Object.keys(this.state.keyboard).map(item => {
             return this.renderInput(item)
         })
     }
