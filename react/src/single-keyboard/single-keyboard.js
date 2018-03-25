2cccc7d085c6b59ccec4f5342f5ee5aaa06b4f16
dbrudner <dbrudne1@gmail.com>
2018-03-25 12:52:51 -0400

    keyboard preview

--- react/src/single-keyboard/single-keyboard.js
+++ react/src/single-keyboard/single-keyboard.js
@@ -7,6 +7,25 @@ import Modal from 'react-responsive-modal'
 
 import {SingleKeyboardContainer} from './single-keyboard-container'
 
+const ImgContainer = styled.div`
+    position: relative;
+    width: 30rem;
+    height: 22rem;
+    display: block;
+    margin: auto;
+
+    img {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        margin: auto;
+        border: 1px solid #c7c4c4;
+        padding: .3rem;
+        cursor: pointer;
+        width: 25rem;
+        margin-left: 2.5rem;
+    }
+`
 
 const imgs = [
     'https://geekhack.org/index.php?action=dlattach;topic=57723.0;attach=63093;image',
@@ -17,11 +36,12 @@ const InfoContainer = styled.div`
     margin: 0 auto;
     display: block;
     width: 30rem;
+    font-size: 1.6rem;
 `
 
 const Arrows = styled.div`
 
-    margin: 1rem 2rem;
+    margin: 0 2rem 0 2.5rem;
     display: flex;
     justify-content: space-between;
 
@@ -33,7 +53,7 @@ const Arrows = styled.div`
 const Header = styled.div`
     margin-bottom: 2rem;
     text-align: center;
-    
+
     h3 {
         font-size: 3rem;
         margin: 0;
@@ -41,7 +61,7 @@ const Header = styled.div`
 
     h5 {
         font-size: 1.8rem;
-        margin-top: 0;
+        margin: 0;
 
         a {
             text-decoration: none;
@@ -89,15 +109,24 @@ class SingleKeyboard extends Component {
     closeModal = () => this.setState({open: false})
 
     showNextImage = () => {
-        if (this.state.showImage > this.state.keyboard.imgs.length - 1) {
+
+        if (this.state.showImage < this.state.keyboard.imgs.length - 1) {
             this.setState({
                 showImage: this.state.showImage + 1
             })
         } else this.setState({showImage: 0})
     }
 
+    showPreviousImage = () => {
+        if (this.state.showImage !== 0) {
+            this.setState({
+                showImage: this.state.showImage - 1
+            })
+        } else this.setState({showImage: this.state.keyboard.imgs.length - 1})
+    }
+
     render() {
-        console.log(this.state.keyboard)
+        console.log(this.state.showImage)
         const keyboard = this.state.keyboard
 
         return (
@@ -110,19 +139,22 @@ class SingleKeyboard extends Component {
                         <h3>Keyboard Name</h3>
                         <h5><Link to='/user/id'>username</Link></h5>
                     </Header>
-                    <img onClick={() => this.setState({open: true})} src={keyboard.imgs[0]} />
                     <Arrows>
-                        <div>
+                        <div onClick={() => this.showPreviousImage()}>
                             <i className="icon fas fa-arrow-left"></i>
                         </div>
-                        <div>                       
+                        <div onClick={() => this.showNextImage()}>
                             <i className="icon fas fa-arrow-right"></i>
                         </div>
                     </Arrows>
-                    <div>Size: {keyboard.size}</div>
-                    <div>Layout: {keyboard.layout}</div>
-                    <div>Switches: {keyboard.switches}</div>
-                    <div>Keycaps: {keyboard.keycaps}</div>
+                    <ImgContainer>
+                        <img onClick={() => this.setState({open: true})} src={keyboard.imgs[this.state.showImage]} />
+                    </ImgContainer>
+                    
+                    <div><strong>Size: </strong>{keyboard.size}</div>
+                    <div><strong>Layout: </strong>{keyboard.layout}</div>
+                    <div><strong>Switches: </strong>{keyboard.switches}</div>
+                    <div><strong>Keycaps: </strong>{keyboard.keycaps}</div>
                 </InfoContainer>
             </SingleKeyboardContainer>
         )
